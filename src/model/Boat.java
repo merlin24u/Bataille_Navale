128172f4f467ea921b110e535f0036163f0fbb07
Quentin Thouvenot <thouven59u@fst-ats-303-l13.fst.site.univ-lorraine.fr>
2018-11-15 15:56:35 +0100

    Completion de Boat.java

--- src/model/Boat.java
+++ src/model/Boat.java
@@ -8,13 +8,62 @@ public class Boat {
 	private int nbCases;
 	private boolean vertical;
 
-	public Boat() {
-
+	public Boat(String name, int life, int nbCases) {
+		this.name = name;
+		this.life = life;
+		this.nbCases = nbCases;
+		this.posX = -1;
+		this.posY = -1;
+	}
+	
+	/**
+	 * Place le bateau,
+	 * si le bateau depasse de la grille ou est placé en dehors, 
+	 * il est placé à la position la plus proche correct  
+	 * @param x 
+	 * @param y
+	 */
+	public void setPos(int x, int y){
+		
+		if(x>= Map.SIZE){
+			x = Map.SIZE-1;
+		}
+		if(x < 0){
+			x = 0;
+		}
+		if(y>= Map.SIZE){
+			y = Map.SIZE-1;
+		}
+		if(y < 0){
+			y = 0;
+		}
+		if(!vertical && Map.SIZE-(x+nbCases)<0)
+			x = Map.SIZE-nbCases;
+		if(vertical && Map.SIZE-(y+nbCases)<0)
+			y = Map.SIZE-nbCases;
+		
+		this.posX = x;
+		this.posY = y;
+	}
+	
+	/**
+	 * indique si le bateu est placé sur la carte
+	 * @return 
+	 * true si le bateau à une position negative 
+	 * false sinon 
+	 */
+	public boolean isPlaced(){
+		return this.posX < 0 || this.posY < 0 ;
+	}
+	
+	public void setVertical(boolean vertical){
+		this.vertical = vertical;
 	}
 
 	public void takeDamage() {
-
+		
 	}
+	
 
 	public boolean isAlive() {
 		return (life > 0);
--- src/model/Map.java
+++ src/model/Map.java
@@ -4,18 +4,28 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 public class Map extends Observable {
-
+	public static final int SIZE = 10;
 	private int nbMissedShot;
 	private int[][] grid;
 	private ArrayList<Boat> listBoat;
 
 	public Map() {
-		grid = new int[10][10];
+		grid = new int[SIZE][SIZE];
 		listBoat = new ArrayList<>();
 	}
 
 	public void attack(int x, int y) {
 
+	}
+	
+	/**
+	 * Place le bateau à l'indice i à la pos x y
+	 * @param i indice dans la liste listBoat
+	 * @param x pos x
+	 * @param y pos y
+	 */
+	public void placer(int i, int x, int y){
+		this.listBoat.get(i).setPos(x, y);
 	}
 
 	public boolean allBoatDestroyed() {

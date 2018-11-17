d256d2d280d51400e4e1da2d9ec5af169b532d61
Paul Merlin <paul.merlin1@etu.univ-lorraine.fr>
2018-11-17 10:28:17 +0100

    petite modif

--- src/model/Map.java
+++ src/model/Map.java
@@ -3,9 +3,6 @@ package model;
 import java.util.ArrayList;
 import java.util.Observable;
 
-import view.ViewMap;
-import view.ViewPlayerMap;
-
 public class Map extends Observable {
 	public static final int SIZE = 10;
 	private int nbMissedShot;
@@ -20,14 +17,18 @@ public class Map extends Observable {
 	public void attack(int x, int y) {
 
 	}
-	
+
 	/**
 	 * Place le bateau à l'indice i à la pos x y
-	 * @param i indice dans la liste listBoat
-	 * @param x pos x
-	 * @param y pos y
+	 * 
+	 * @param i
+	 *            indice dans la liste listBoat
+	 * @param x
+	 *            pos x
+	 * @param y
+	 *            pos y
 	 */
-	public void placer(int i, int x, int y){
+	public void placer(int i, int x, int y) {
 		this.listBoat.get(i).setPos(x, y);
 	}
 

b28b69c43e55fcd3438f58c87e89fe4b269b675a
Paul Merlin <=>
2018-12-10 16:51:47 +0100

    modif texture

--- src/client/texture/TextureFactory.java
+++ src/client/texture/TextureFactory.java
@@ -1,24 +1,25 @@
 package client.texture;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class TextureFactory {
 
 	public static TextureFactory instance;
-	private BufferedImage cruiser, cruiser_Hor, patrolShip, patrolShip_Hor, galleon, galleon_Hor, caravel, caravel_Hor;
+	private BufferedImage cruiser, cruiser_Hor, patrolShip, patrolShip_Hor,
+			galleon, galleon_Hor, caravel, caravel_Hor;
 
 	private TextureFactory() throws IOException {
-		cruiser = ImageIO.read(new File("res/XXe/cruiser.png"));
-		cruiser_Hor = ImageIO.read(new File("res/XXe/cruiser_Hor.png"));
-		patrolShip = ImageIO.read(new File("res/XXe/patrolShip.png"));
-		patrolShip_Hor = ImageIO.read(new File("res/XXe/patrolShip_Hor.png"));
-		galleon = ImageIO.read(new File("res/XVIe/galleon.png"));
-		galleon_Hor = ImageIO.read(new File("res/XVIe/galleon_Hor.png"));
-		caravel = ImageIO.read(new File("res/XVIe/caravel.png"));
-		caravel_Hor = ImageIO.read(new File("res/XVIe/caravel_Hor.png"));
+		cruiser = ImageIO.read(ResourceLoader.load("XXe/cruiser.png"));
+		cruiser_Hor = ImageIO.read(ResourceLoader.load("XXe/cruiser_Hor.png"));
+		patrolShip = ImageIO.read(ResourceLoader.load("XXe/patrolShip.png"));
+		patrolShip_Hor = ImageIO.read(ResourceLoader
+				.load("XXe/patrolShip_Hor.png"));
+		galleon = ImageIO.read(ResourceLoader.load("XVIe/galleon.png"));
+		galleon_Hor = ImageIO.read(ResourceLoader.load("XVIe/galleon_Hor.png"));
+		caravel = ImageIO.read(ResourceLoader.load("XVIe/caravel.png"));
+		caravel_Hor = ImageIO.read(ResourceLoader.load("XVIe/caravel_Hor.png"));
 	}
 
 	public BufferedImage getTexture(String texture) throws Exception {
